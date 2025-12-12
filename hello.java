458d57d74c019753eea87223640179ead599c3e7
Sivasaiteja2 <gurramsai02@gmail.com>
2025-12-12 17:51:10 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class HelloSai {
+    public static void main(String[] args) {
+        System.out.println("Hello Sai, how are you?");
+        System.out.println("This is my first Jenkins code.");
+    }
+}
