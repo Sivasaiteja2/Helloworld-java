28438b48f205deddad680a661162442f30abeba6
Siva sai teja <gurramsai02@gmail.com>
2025-12-16 11:56:17 +0530

    Update HelloSai.java

--- HelloSai.java
+++ HelloSai.java
@@ -1,7 +1,8 @@
 public class HelloSai {
     public static void main(String[] args) {
-        System.out.println("Hello Sai,  how are you?");
+        System.out.println("Hello Sai how are you?");
         System.out.println("This is my first Jenkins code.");
         System.out.println("This is my second Jenkinscode.");
+        
     }
 }
